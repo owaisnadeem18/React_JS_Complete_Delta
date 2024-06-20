bc511609570fc11bc2cb859c6b4f3f2195875a5a
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-06-21 01:31:21 +0500

    Add files via upload

--- React JS Part 04/to-do-list/src/App.jsx
+++ React JS Part 04/to-do-list/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import Todo from "./Components/Todo";
+// import Todo from "./Components/Todo";
+import { Todo_Repractice } from "./Components/Todo_Repractice";
 
 function App() {
   return (
     <>
-      <Todo />
+      {/* <Todo /> */}
+      <Todo_Repractice />
     </>
   );
 }
